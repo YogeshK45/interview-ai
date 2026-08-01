7d9b2d90cf0f2c54bc35160b220f7e3904f1f28a
yogesh <yogeshrajput08273@gmail.com>
2026-08-01 20:49:59 +0530

    Use env variable for API

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * Axios instance with baseline configurations
  */
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
@@ -17,7 +17,7 @@ export const generateInterviewReport = async ({ jobDescription, selfDescription,
     formData.append("jobDescription", jobDescription);
     formData.append("selfDescription", selfDescription);
     formData.append("preparationDuration", preparationDuration);
-    
+
     if (resumeFile) {
         formData.append("resume", resumeFile);
     }
